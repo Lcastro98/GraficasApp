d39f95a980191e069662ab4f7dbbda33ee567d89
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-11-07 17:41:45 -0500

    Añadir flexibilidad al componente personalizado

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
@@ -9,54 +9,22 @@ import { BaseChartDirective } from 'ng2-charts';
   ]
 })
 export class GraficaBarraComponent {
+
+  @Input() horizontal: boolean = false;
+
+  @Input() barChartData!: ChartData<'bar'>;
+
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
  
   public barChartOptions: ChartConfiguration['options'] = {
     responsive: true,
   };
   public barChartType: ChartType = 'bar';
-  public barChartData: ChartData<'bar'> = {
-    labels: ['2020', '2021', '2022', '2023', '2024', '2025', '2026'],
-    datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#ED5F76' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#F763C4' },
-      { data: [38, 10, 24, 60, 89, 12, 70], label: 'Series C', backgroundColor: '#D665E0' },
-    ],
-  };
  
-  public randomize(): void {
-    // Only Change 3 values
-    this.barChartData.datasets[0].data = [
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100)
-    ];
-
-    this.barChartData.datasets[1].data = [
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100)
-    ];
-
-    this.barChartData.datasets[2].data = [
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100)
-    ];
- 
-    this.chart?.update();
+  ngOnInit(): void {
+    if(this.horizontal) {
+      this.barChartOptions!.indexAxis = 'y';
+    }
   }
 
 }
