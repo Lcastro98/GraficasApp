7ff9fec90d8d62137a4038e631a36a70be7acbf5
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-11-07 20:49:08 -0500

    Grafica de Dona mediante peticion http

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GraficasService } from '../../services/graficas.service';
 
 @Component({
   selector: 'app-dona-http',
@@ -8,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonaHttpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private graficasServices: GraficasService) { }
 
   ngOnInit(): void {
+
+    this.graficasServices.getUsuariosRedesSociales()
+      .subscribe( data => {
+        console.log(data);
+      });
   }
 
 }
